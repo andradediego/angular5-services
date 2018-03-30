4d54974715a2ac0d92538b3ceec243f2fbce6bec
Diego <andrade.diego@outlook.com>
2018-03-30 11:12:11 -0300

    added build to Git hub

--- src/app/Services/accounts.service.ts
+++ src/app/Services/accounts.service.ts
@@ -17,16 +17,16 @@ export class AccountsService {
       status: 'unknown'
     }
   ];
-  constructor(private logginService: LoggingService){}
+  constructor(private loggingService: LoggingService){}
 
   addAccount(name: string, status: string){
     this.accounts.push({name: name, status: status});
-    this.logginService.logStatusChange(status);
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string){
     this.accounts[id].status = status;
-    this.logginService.logStatusChange(status);
+    this.loggingService.logStatusChange(status);
   }
 
 
